8a55a89ec28d72f7b73271c7bc0568d41e9a73f6
Brayan_Alves <brayanalvesdossantos67@gmail.com>
2026-06-15 18:25:42 -0300

    Preparação do ambiente do projeto

---
+++ src/main/java/br/edu/ifpr/aquacare/AquacareApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.aquacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquacareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpr/aquacare/AquacareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.aquacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquacareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
