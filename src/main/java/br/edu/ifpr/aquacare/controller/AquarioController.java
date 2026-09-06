6ed7cff3b8885dbd771ff23cfbfe57c4e02991e3
Brayan_Alves <brayanalvesdossantos67@gmail.com>
2026-09-06 19:08:34 -0300

    mudança no aquario controller

--- src/main/java/br/edu/ifpr/aquacare/controller/AquarioController.java
+++ src/main/java/br/edu/ifpr/aquacare/controller/AquarioController.java
@@ -5,12 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import br.edu.ifpr.aquacare.entity.Aquario;
 import br.edu.ifpr.aquacare.enums.NivelPlanta;
 import br.edu.ifpr.aquacare.enums.TipoAgua;
 import br.edu.ifpr.aquacare.service.AquarioService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/aquarios")
@@ -33,7 +33,7 @@ public class AquarioController {
         aquario.setTipoAgua(dados.tipoAgua());
         aquario.setNivelPlantas(dados.nivelPlanta());
         aquario.setCiclado(dados.isCiclado());
-        aquario.setInjecaoCO2(dados.isCiclado());
+        aquario.setInjecaoCO2(dados.injecaoCO2());
 
         aquarioService.cadastrar(aquario);
         return ResponseEntity.status(HttpStatus.CREATED).build();
